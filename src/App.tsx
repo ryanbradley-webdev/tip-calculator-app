946d271a7c9c5069fca2d2f6b26212f3afcc85f9
Ryan Bradley <ryanbradley315@gmail.com>
2023-10-03 19:29:37 -0500

    Add basic form content.

--- src/App.tsx
+++ src/App.tsx
@@ -15,9 +15,9 @@ function App() {
 
           <label htmlFor="bill">
 
-            <span>
+            <h4>
               Bill
-            </span>
+            </h4>
 
             <input
               type="text"
@@ -27,11 +27,121 @@ function App() {
 
           </label>
 
+          <div>
+
+            <h4>
+              Select Tip %
+            </h4>
+
+            <label htmlFor="five">
+              <span>
+                5%
+              </span>
+
+              <input type="radio" name="percent" id="five" />
+            </label>
+            
+            <label htmlFor="ten">
+              <span>
+                10%
+              </span>
+
+              <input type="radio" name="percent" id="ten" />
+            </label>
+            
+            <label htmlFor="fifteen">
+              <span>
+                15%
+              </span>
+
+              <input type="radio" name="percent" id="fifteen" />
+            </label>
+            
+            <label htmlFor="twentyfive">
+              <span>
+                25%
+              </span>
+
+              <input type="radio" name="percent" id="twentyfive" />
+            </label>
+            
+            <label htmlFor="fifty">
+              <span>
+                50%
+              </span>
+
+              <input type="radio" name="percent" id="fifty" />
+            </label>
+            
+            <label htmlFor="custom">
+
+              <span>
+                Custom
+              </span>
+
+              <input type="radio" name='percent' id='custom' />
+
+            </label>
+
+          </div>
+
+          <label htmlFor="people">
+
+            <h4>
+              Number of People
+            </h4>
+
+            <input type="number" name="people" id="people" />
+
+          </label>
+
         </div>
 
         <div>
 
+          <div>
 
+            <div>
+
+              <h3>
+                Tip Amount
+              </h3>
+
+              <p>
+                / person
+              </p>
+
+            </div>
+
+            <h2>
+              $4.27
+            </h2>
+
+          </div>
+
+          <div>
+
+            <div>
+
+              <h3>
+                Total
+              </h3>
+
+              <p>
+                / person
+              </p>
+
+            </div>
+
+            <h2>
+              $32.79
+            </h2>
+
+          </div>
+
+          <button>
+            RESET
+          </button>
 
         </div>
 
